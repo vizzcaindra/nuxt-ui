ec3f70e80c6150a84d4cffa3d33cd73e1733d50b
Vizzca Indra P <vizzcaindra@gmail.com>
2022-10-28 09:21:27 +0700

    Changed: Nuxt UI component implement mixins feature.

--- src/runtime/components/container/Container.vue
+++ src/runtime/components/container/Container.vue
@@ -1,8 +1,17 @@
 <template>
-
+  <div>
+    <slot />
+  </div>
 </template>
 
 <script>
+
+import Component from "@nuxt-ui/ui/Component";
+
+class Container extends Component {
+
+}
+
 export default {
   name: "Container"
 }
