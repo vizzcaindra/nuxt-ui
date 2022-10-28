8e3f63e654d17ad620bf0ababd2a5434cfaf5a82
Vizzca Indra P <vizzcaindra@gmail.com>
2022-10-28 09:24:41 +0700

    Added: Base component for Nuxt UI

---
+++ src/runtime/ui/Component.ts
@@ -0,0 +1,12 @@
+/*
+ * Vixio
+ * Copyright © 2022 - Nuxt UI Contributor
+ * Created by Vizzca Indra Pratama
+ * Created at 2022-10-27
+ */
+
+import Base from "./Base";
+
+export default class Component extends Base {
+  public hidden: boolean = false;
+}
