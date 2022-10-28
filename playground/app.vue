3fc33d37000bbad0dcf15da237cf36f4bdcca0f4
Vizzca Indra P <vizzcaindra@gmail.com>
2022-10-28 09:22:27 +0700

    Changed: Simplify app.vue structure

--- playground/app.vue
+++ playground/app.vue
@@ -1,9 +1,11 @@
 <template>
-  <div>
-    Nuxt module playground!
-    <nuxt-buttonButton>Test</nuxt-buttonButton>
-  </div>
+  <nuxt-container class="fixed inset-0 flex justify-center sm:px-8">
+    <div class="flex w-full max-w-7xl lg:px-8">
+      <div class="w-full bg-white dark:bg-zinc-900" />
+    </div>
+  </nuxt-container>
+  <nuxt-container class="relative m-5">
+    <nuxt-button>Button Example</nuxt-button>
+    <nuxt-button class="ml-3">Button Example</nuxt-button>
+  </nuxt-container>
 </template>
-
-<script setup>
-</script>
